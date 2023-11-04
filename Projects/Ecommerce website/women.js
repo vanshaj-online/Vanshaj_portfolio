ca91849e0ea6fa9d068798f5f0b1f689d293188b
vanshaj-online <singhvanshaj09@gmail.com>
2023-11-04 11:45:09 +0530

    completed women.html and women.js

--- Projects/Ecommerce website/women.js
+++ Projects/Ecommerce website/women.js
@@ -1,22 +1,30 @@
-let scroller = document.querySelector('.cat-prod'); 
-const btn = document.querySelectorAll('.btn');
-btn.forEach((elem)=>{
-    elem.addEventListener('click',function(){
-        scroller.style.transform = 'translateX(-100%)';
-    })
-})
+let scroller = document.querySelectorAll('.cat-prod'); 
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+let lastelem = document.querySelector('#last');
+let check = 'hidden' 
 const observer = new IntersectionObserver(entries =>{
     entries.forEach(last => {
-        if(last.isIntersecting === true){
-            console.log('yes',last.target)
-        }else{
-            console.log('no',last.target)
+        if(last.intersectionRatio !== 1 ){
+            next.addEventListener('click',function(){
+                scroller.forEach(elem => {
+                    elem.style.transition = '.2s linear'
+                    elem.style.transform = 'translateX(-100%)'
+                })
+                next.classList.remove('show')
+                prev.classList.add('show')
+            })
+            check = 'shown'
         }
-    }),{
-        threshold : 1
-    }
+    })
+    prev.addEventListener('click', function () {
+        scroller.forEach(elem => {
+            elem.style.transition = '.2s linear'
+            elem.style.transform = 'translateX(0%)'
+        })
+        prev.classList.remove('show')
+        next.classList.add('show')
+        console.log('clicked')
+    })
 })
-
-let lastelem = document.querySelector('#last');
-
 observer.observe(lastelem);

7acefc14e3b2a8a7fbf2598879638ae0af8fac04
vanshaj-online <singhvanshaj09@gmail.com>
2023-11-05 23:06:15 +0530

    added login page.. not completed

--- Projects/Ecommerce website/women.js
+++ Projects/Ecommerce website/women.js
@@ -1,44 +1,61 @@
-let scroller = document.querySelectorAll('.cat-prod'); 
-const prev = document.querySelector('.prev');
-const next = document.querySelector('.next');
-let lastelem = document.querySelector('#last');
-let check = 'hidden' ;
-var hovers = document.querySelectorAll('.hover');
-var main = document.querySelector('#main')
-const observer = new IntersectionObserver(entries =>{
-    entries.forEach(last => {
-        if(last.intersectionRatio !== 1 ){
-            next.addEventListener('click',function(){
-                scroller.forEach(elem => {
-                    elem.style.transition = '.2s linear'
-                    elem.style.transform = 'translateX(-100%)'
+function scroll(){
+    let scroller = document.querySelectorAll('.cat-prod');
+    const prev = document.querySelector('.prev');
+    const next = document.querySelector('.next');
+    let lastelem = document.querySelector('#last');
+    let check = 'hidden';
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(last => {
+            if (last.intersectionRatio !== 1) {
+                next.addEventListener('click', function () {
+                    scroller.forEach(elem => {
+                        elem.style.transition = '.2s linear'
+                        elem.style.transform = 'translateX(-100%)'
+                    })
+                    next.classList.remove('show')
+                    prev.classList.add('show')
                 })
-                next.classList.remove('show')
-                prev.classList.add('show')
-            })
-            check = 'shown'
-        }
-    })
-    prev.addEventListener('click', function () {
-        scroller.forEach(elem => {
-            elem.style.transition = '.2s linear'
-            elem.style.transform = 'translateX(0%)'
+                check = 'shown'
+            }
         })
-        prev.classList.remove('show')
-        next.classList.add('show')
-        console.log('clicked')
+        prev.addEventListener('click', function () {
+            scroller.forEach(elem => {
+                elem.style.transition = '.2s linear'
+                elem.style.transform = 'translateX(0%)'
+            })
+            prev.classList.remove('show')
+            next.classList.add('show')
+            console.log('clicked')
+        })
     })
-})
-observer.observe(lastelem);
+    observer.observe(lastelem);
+}
+scroll()
 
+function dropdown(){
+    var hovers = document.querySelectorAll('.hover');
+    var main = document.querySelector('#main')
+    hovers.forEach(elem => {
+        elem.addEventListener('mousemove', function () {
+            elem.classList.add('show')
+            main.style.filter = 'blur(2px)'
+        })
+        elem.addEventListener('mouseleave', function () {
+            elem.classList.remove('show')
+            main.style.filter = 'blur(0)'
+        })
+    })
+}
+dropdown()
 
-hovers.forEach(elem => {
-    elem.addEventListener('mousemove', function () {
-        elem.classList.add('show')
-        main.style.filter = 'blur(2px)'
+function dropdownprofile() {
+    var loginbtn = document.querySelector('.profile')
+    var loginhvr = document.querySelector('.hvr')
+    loginbtn.addEventListener('mousemove', () => {
+        loginhvr.classList.add('active')
     })
-    elem.addEventListener('mouseleave', function () {
-        elem.classList.remove('show')
-        main.style.filter = 'blur(0)'
+    loginbtn.addEventListener('mouseleave', () => {
+        loginhvr.classList.remove('active')
     })
-})
+}
+dropdownprofile()

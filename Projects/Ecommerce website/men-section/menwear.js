3d70b5387bcf6e5f43007e5bbaa5d14bca918df0
vanshaj-online <singhvanshaj09@gmail.com>
2023-11-08 23:11:42 +0530

    added filter functionality in menwear.js

--- Projects/Ecommerce website/men-section/menwear.js
+++ Projects/Ecommerce website/men-section/menwear.js
@@ -29,14 +29,91 @@ function dropdownprofile() {
 }
 dropdownprofile()
 
-function filter(){
+function filterProduct() {
     let products = document.querySelectorAll('.shop-card');
-    // console.log(products)
-    products.forEach(elem =>{
-        if (!elem.classList.contains('tshirt')){
-            elem.classList.add('hidden')
-            console.log(elem)
-        }
+    function removeHidden(){
+        products.forEach(elem =>{
+            if (elem.classList.contains('hidden')) {
+                elem.classList.remove('hidden')
+            }
+        })
+    }
+    function tshirtFilter() {
+        products.forEach(elem => {
+            if (!elem.classList.contains('tshirt')) {
+                elem.classList.add('hidden')
+                console.log(elem)
+            }
+        })
+    }
+    function shirtFilter() {
+        products.forEach(elem => {
+            if (!elem.classList.contains('shirt')) {
+                elem.classList.add('hidden')
+                console.log(elem)
+            }
+        })
+    }
+    function jacketFilter() {
+        products.forEach(elem => {
+            if (!elem.classList.contains('jacket')) {
+                elem.classList.add('hidden')
+                console.log(elem)
+            }
+        })
+    }
+    function bottomwearFilter() {
+        products.forEach(elem => {
+            if (!elem.classList.contains('jeans')) {
+                elem.classList.add('hidden')
+                console.log(elem)
+            }
+        })
+    }
+    function shoesFilter() {
+        products.forEach(elem => {
+            if (!elem.classList.contains('shoes')) {
+                elem.classList.add('hidden')
+                console.log(elem)
+            }
+        })
+    }
+    function accessoriesFilter() {
+        products.forEach(elem => {
+            if (!elem.classList.contains('accessories')) {
+                elem.classList.add('hidden')
+                console.log(elem)
+            }
+        })
+    }
+
+    document.querySelector('#all').addEventListener('click', function () {
+        removeHidden()
     })
+    document.querySelector('#tshirt').addEventListener('click', function () {
+        removeHidden()
+        tshirtFilter()
+    })
+    document.querySelector('#shirt').addEventListener('click', function () {
+        removeHidden()
+        shirtFilter()
+    })
+    document.querySelector('#jacket').addEventListener('click', function () {
+        removeHidden()
+        jacketFilter()
+    })
+    document.querySelector('#jeans').addEventListener('click', function () {
+        removeHidden()
+        bottomwearFilter()
+    })
+    document.querySelector('#shoes').addEventListener('click', function () {
+        removeHidden()
+        shoesFilter()
+    })
+    document.querySelector('#accessories').addEventListener('click', function () {
+        removeHidden()
+        accessoriesFilter()
+    })
+
 }
-// filter()
+filterProduct()
